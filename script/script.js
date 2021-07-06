dff3fd4ca57f223ae515267ff5b8598a225b0c62
Gustavo Romero Cañas <romeroc.gus@gmail.com>
2021-07-06 23:02:44 +0200

    change softskills imgs, added text

--- script/script.js
+++ script/script.js
@@ -1,109 +1,8 @@
+//****Biography navigation ****//
 const btnBack = document.querySelector("#bio__btn-back");
 const btnFwd = document.querySelector("#bio__btn-fwd");
 const bioDots = document.querySelectorAll(".bio__nav-dot");
 const bioText = document.querySelector(".bio__text");
-const skillText = document.querySelector(".skills__text");
-const softText = document.querySelector(".softSkills__text");
-const skills = document.querySelectorAll(".flex-skillitem");
-const softSkills = document.querySelectorAll(".flex-softSkillitem");
-const projects = document.querySelectorAll(".grid-projects__item");
-const aside = document.querySelector(".flex-aside");
-
-let skillTextAsign = ()=>{
-    for (i = 0; i< skills.length; i++){
-        let newText = "";
-        if(i == 0){
-            newText = `HTML:<br>I'm confident in the use of Html to structure the document with good semantics in order to boost your SEO.`;
-        }else if(i == 1){
-            newText = `CSS:<br>I like to keep up with every fuctionality that Css brings. I work with Grid, Flexbox, animations, pseudo-elements and more to make your wishes come true!`;
-        }else if(i == 2){
-            newText = `Javascript:<br>I love programming and find creative solutions. I'm confident in the use of Javascript Vanilla, and also have experience using some frameworks too.`;
-        }else if(i == 3){
-            newText = `GIT:<br>I use Git as version control sistem for my projects.<br>`;
-        }else if(i == 4){
-            newText = `Responsive Design:<br>All my Apps can adapt to any device you've got!`;
-        }else if(i == 5){
-            newText = `Visual Studio Code:<br>This is my editor of choice. I'm pretty used to it, and love working with.`;
-        }else if(i == 6){
-            newText = `Bootstrap:<br>I rather work with pure CSS, but i have experiencie using bootstrap and feel confident with it too.`;
-        }else if(i == 7){
-            newText = `Photoshop:<br>I use it as a tool to boost up my images and make the things even more beatiful.`;
-        }else if(i == 8){
-            newText = `Illustrator:<br>I have some experience with it, making my own designs when needed.`;
-        };
-
-        skills[i].addEventListener("click", ()=>{
-            skillText.innerHTML = newText;
-        });
-    };
-};
-
-let softSkillTextAsign = ()=>{
-    for (i = 0; i< softSkills.length; i++){
-        let newText = "";
-        if(i == 0){
-            newText = "el texto de tw";
-        }else if(i == 1){
-            newText = "el texto de fast";
-        }else if(i == 2){
-            newText = "el texto de nice";
-        }else if(i == 3){
-            newText = "el texto de positive";
-        }else if(i == 4){
-            newText = "el texto de funny";
-        }else if(i == 5){
-            newText = "el texto de vsCode";
-        }else if(i == 6){
-            newText = "el texto de bootstrap";
-        }else if(i == 7){
-            newText = "el texto de photoshop";
-        }else if(i == 8){
-            newText = "el texto de illustrator";
-        };
-
-        softSkills[i].addEventListener("click", ()=>{
-            softText.innerHTML = newText;
-        });
-    };
-};
-
-let asideMaker = ()=> {
-    for (i=0; i<projects.length; i++){
-        let text;
-        if(i == 0){
-            text= `<h3 class="card__title">Proyecto 1</h3>
-			<p class="card__text">Texto del proyecto 1</p>`;
-            projects[i].addEventListener("click", ()=>{
-                aside.classList.add("aside-visible");
-                aside.classList.remove("even");
-                aside.innerHTML = text;
-            });
-        } else if(i == 1) {
-            text= `<h3 class="card__title">Proyecto 2</h3>
-			<p class="card__text">Texto del proyecto 2</p>`;
-            projects[i].addEventListener("click", ()=>{
-                aside.classList.add("aside-visible", "even");
-                aside.innerHTML = text;
-            });
-        } else if(i == 2){
-            text= `<h3 class="card__title">Proyecto 3</h3>
-			<p class="card__text">Texto del proyecto 3</p>`;
-            projects[i].addEventListener("click", ()=>{
-                console.log(i);
-                aside.classList.add("aside-visible");
-                aside.classList.remove("even");
-                aside.innerHTML = text;
-            });
-        } else if(i == 3) {
-            text= `<h3 class="card__title">Proyecto 4</h3>
-			<p class="card__text">Texto del proyecto 4</p>`;
-            projects[i].addEventListener("click", ()=>{
-                aside.classList.add("aside-visible", "even");
-                aside.innerHTML = text;
-            });
-        };
-    }
-}
 
 let bioTextMaker = ()=>{
     let texto1 = `I started my career with the dream of give to the world the most unique experiences in every project that i contribute to make. I really enjoy develop, design and experiment with <span class="colors">colors</span>, <span class="fonts">fonts</span>, <span class="anime">animations</span> and every component that makes a difference to give me the posibility of bring to life UIs that people will love to use!`;
@@ -154,13 +53,107 @@ let bioTextMaker = ()=>{
     });
 
 }
-skillTextAsign();
-softSkillTextAsign();
-asideMaker();
 bioTextMaker();
 
-// intersection observers
-// Nav
+//****soft skills text generator****//
+const softText = document.querySelector(".softSkills__text");
+const softSkills = document.querySelectorAll(".flex-softSkillitem");
+
+let softSkillTextAsign = ()=>{
+    softSkills.forEach(softSkill =>{
+        let newText;
+        if(softSkill.firstElementChild.innerHTML == "Team Worker"){
+            newText = "Team Worker:<br>i think that we are better if we work together!<br> i like to work with people to inspire and be inspired, share ideas and learn of each other."
+        } else if(softSkill.firstElementChild.innerHTML == "Fast Learner"){
+            newText = "Fast Learner:<br>I love to learn new things, the feels of improvement, and the satisfaction of unlock a new skill.<br> As i enjoy it so much, i tend to involve a lot and learn fast."
+        } else if(softSkill.firstElementChild.innerHTML == "Friendly"){
+            newText = "Friendly:<br>That's it. I'm usually in a good mood, active listener, like to chat and you can count on me pretty much for everything."
+        } else if(softSkill.firstElementChild.innerHTML == "Positive"){
+            newText = "Positive:<br>I like to see the bright side of thing, if there is bad moments i try to cheer things up to raise the moral of the team."
+        } else if(softSkill.firstElementChild.innerHTML == "Creative"){
+            newText = "Creative:<br>If there's something about this job that i like the most is that we can make reality our dreams!. I like to try different ways to make things happen, and bring creative solutions or ideas to play with."
+        } else if(softSkill.firstElementChild.innerHTML == "Meticulous"){
+            newText = "Meticulous:<br>I have so much attention for details. I have no problem to do and re-do things in order to make them look the way you like or improve my code to make things more legible, more efficient, prettier."
+        }
+
+        softSkill.addEventListener("click", ()=>{
+            softText.innerHTML = newText;
+        });
+    });
+    // for (i = 0; i< softSkills.length; i++){
+    //     let newText = "";
+    //     if(i == 0){
+    //         newText = "el texto de tw";
+    //     }else if(i == 1){
+    //         newText = "el texto de fast";
+    //     }else if(i == 2){
+    //         newText = "el texto de nice";
+    //     }else if(i == 3){
+    //         newText = "el texto de positive";
+    //     }else if(i == 4){
+    //         newText = "el texto de funny";
+    //     }else if(i == 5){
+    //         newText = "el texto de vsCode";
+    //     }else if(i == 6){
+    //         newText = "el texto de bootstrap";
+    //     }else if(i == 7){
+    //         newText = "el texto de photoshop";
+    //     }else if(i == 8){
+    //         newText = "el texto de illustrator";
+    //     };
+
+    //     softSkills[i].addEventListener("click", ()=>{
+    //         softText.innerHTML = newText;
+    //     });
+    // };
+};
+softSkillTextAsign();
+
+//****Projects Info modal ****//
+const projects = document.querySelectorAll(".grid-projects__item");
+const aside = document.querySelector(".flex-aside");
+
+let asideMaker = ()=> {
+    for (i=0; i<projects.length; i++){
+        let text;
+        if(i == 0){
+            text= `<h3 class="card__title">Proyecto 1</h3>
+			<p class="card__text">Texto del proyecto 1</p>`;
+            projects[i].addEventListener("click", ()=>{
+                aside.classList.add("aside-visible");
+                aside.classList.remove("even");
+                aside.innerHTML = text;
+            });
+        } else if(i == 1) {
+            text= `<h3 class="card__title">Proyecto 2</h3>
+			<p class="card__text">Texto del proyecto 2</p>`;
+            projects[i].addEventListener("click", ()=>{
+                aside.classList.add("aside-visible", "even");
+                aside.innerHTML = text;
+            });
+        } else if(i == 2){
+            text= `<h3 class="card__title">Proyecto 3</h3>
+			<p class="card__text">Texto del proyecto 3</p>`;
+            projects[i].addEventListener("click", ()=>{
+                console.log(i);
+                aside.classList.add("aside-visible");
+                aside.classList.remove("even");
+                aside.innerHTML = text;
+            });
+        } else if(i == 3) {
+            text= `<h3 class="card__title">Proyecto 4</h3>
+			<p class="card__text">Texto del proyecto 4</p>`;
+            projects[i].addEventListener("click", ()=>{
+                aside.classList.add("aside-visible", "even");
+                aside.innerHTML = text;
+            });
+        };
+    }
+}
+asideMaker();
+
+//****intersection observers****//
+// Nav //
 const navItem = document.querySelectorAll(".navbar__item");
 const sections = document.querySelectorAll(".section");
 const navOptions = {
@@ -202,7 +195,7 @@ sections.forEach(section=>{
     navScroll.observe(section);
 });
 
-//Faders Start
+// Faders //
 const fadeBot = document.querySelectorAll(".fade-bot");
 const fadeRight = document.querySelectorAll(".fade-right");
 const fadeLeft = document.querySelectorAll(".fade-left");
@@ -229,4 +222,3 @@ fadeRight.forEach(fader =>{
 fadeLeft.forEach(fader =>{
     appearOnScroll.observe(fader);
 });
-//Faders End
